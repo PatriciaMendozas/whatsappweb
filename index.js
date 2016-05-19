7b0ac70ec5f111648a52039afa2828a28d3ac3cc
PatriciaMendozas <patriciamendozasoncco@gmail.com>
2016-05-19 11:44:59 -0500

    listoooo

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http= require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req,res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('El profe esta enojado');
+	socket.on('chat', function(_msg){
+		//console.log(_msg);
+		io.emit('nuevo_mensaje',_msg);
+	});
+});
+
+
+http.listen(8080,function(){
+	console.log('Express run on port 8080');
+});
